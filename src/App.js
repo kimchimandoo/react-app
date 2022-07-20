51b1cecc8e65ec8b9f167e3a5f3a572b5cd61f9f
Hanna <hanrankim@gmail.com>
2022-07-20 15:11:02 -0700

    updated file

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return <header>
+  <h1><a href='/'>Web</a></h1>
+</header>
+}
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <nav>
+        <ol>
+          <li><a href='/read/1'>HTML</a></li>
+          <li><a href='/read/2'>CSS</a></li>
+          <li><a href='/read/3'>JS</a></li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+      </article>
+    </div>
+  );
+}
+
+export default App;
